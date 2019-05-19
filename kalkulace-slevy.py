58cd218da42158d2bd3dd546fc190dc70f85fc15
Jan Bures <info@janbures.com>
2019-05-19 19:28:30 +0200

    clean up

--- kalkulace-slevy.py
+++ kalkulace-slevy.py
@@ -1,4 +1,4 @@
-
+slevaSTR = 0
 
 def strToFloatPrice(n):
     """
@@ -15,26 +15,39 @@ def strToFloatPrice(n):
         print("chyba")
     return cislo
 
+
 def getPrice():
-    cena = 0.0
-    cenaCLR = 0.0
-    cena = strToFloatPrice(cenaSTR)
+    """
+    získej cenu produktu od uživatele
+    :return: cena produktu float
+    """
+    cenaSTR = 0.0
     cenaSTR = input("zadej cenu:").strip()
-    return cena
+    cenaSTR = float(cenaSTR)
+    return cenaSTR
 
 
 def getDiscount():
+    """
+    získej výši slevy od uživatele
+    :return: výše slevy float
+    """
     slevaSTR = 0.0
     slevaSTR = input("zadej slevu v procentech:").strip()
     slevaFL = float(slevaSTR)
     return slevaFL
 
+
 def spocitejSlevu():
-    cena = getPrice()
-    sleva = getDiscount()
+    cena = getPrice()           # získej cenu produktu od uživatele
+    sleva = getDiscount()       # získej výši slevy od uživatele
+    vyse_slevy = cena - sleva   # sleva vyšíslena v měně
     poSleve = (cena * ((100-sleva)*0.01))
-    sdeleni = "částka {}Kč po {}% slevě je {}Kč".format(cena, round(sleva), poSleve)
+    sdeleni = "částka {}Kč po {}% slevě je {}Kč".format(cena, sleva, round(poSleve,2))
+
+
     print(sdeleni)
+    print("Zlevněno o ", vyse_slevy)
 
 spocitejSlevu()
 
