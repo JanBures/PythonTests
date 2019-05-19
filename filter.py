89e5f6698b6e0c2b19cdcce2c09b52b31e8f0700
Jan Bures <info@janbures.com>
2019-05-19 10:28:14 +0200

    translation

--- filter.py
+++ filter.py
@@ -1,14 +1,14 @@
 
 #[-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-cisla = [x for x in range(-10,10)]
-#vytiskni cisla
-print(cisla)
+nums = [x for x in range(-10, 10)]
+#print nums
+print(nums)
 
 
 """
 filtr 
 """
-# odfiltrujce čísla větší než 0
-f = list(filter(lambda x:x<0, cisla))
+# filter out nums >=0
+f = list(filter(lambda x:x<=0, nums))
 
 print(f)
