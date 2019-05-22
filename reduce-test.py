86e3dee714a93c811d90faf14ed924edeec9a628
Jan Bures <info@janbures.com>
2019-05-22 20:09:02 +0200

    exception handling

--- reduce-test.py
+++ reduce-test.py
@@ -8,6 +8,7 @@ to compute the product of a list of integers.
 
 # list of nums [1, 10, 19, 28, 37, 46, 55, 64, 73, 82, 91]
 nums = [x for x in range(1,100,9)]
+
 print(nums)
 
 # sum all list items
@@ -19,6 +20,10 @@ y = sum(nums)
 print("sum function: ",y)
 
 
+nums.append("abc")  # exception TypeError raise test
+nums.append("def")  # exception TypeError raise test
+nums.append("ghi")  # exception TypeError raise test
+
 # same solution with function
 def sumar(x):
     """
@@ -27,8 +32,14 @@ def sumar(x):
     :return: int sumarized input parameter
     """
     suma = 0
+    err_counter = 0
     for i in x:
-        suma+=i
+        try:
+            suma+=i
+        except TypeError:   # if values are not numbers
+            err_counter +=1
+            continue
+    print("počet chyb:", err_counter)
     return suma
 
 print("same solution with function sumar():",sumar(nums))
