cb89214597012666d5906580bfd4bc23eae4a13b
Jan Bures <info@janbures.com>
2019-05-28 00:01:25 +0200

    new tests with json

---
+++ json_handler.py
@@ -0,0 +1,14 @@
+import json
+
+data = {
+    "Jmeno": "Pavel",
+    "Auto":[{"BMW":1.5},{"BMW":3.7}],
+    "Deti":True,
+    "Jmena deti":["Matouš","Pavel","Lucie"],
+    "Zenaty":True,
+    "Manzelka":"Marie"
+}
+
+print(data)
+
+print(json.dumps(data, indent=4))
