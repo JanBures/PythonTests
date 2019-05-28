69b0eebf3359a8fd39a55aa29297fc678268a8a2
Jan Bures <info@janbures.com>
2019-05-28 20:24:59 +0200

    edits

--- fibon.py
+++ fibon.py
@@ -1,10 +1,17 @@
-def fibon(start):
-    a = 1
-    b = 1
-    for i in range(start,100):
+def fibon(start,amount):
+    """
+    Fibonacci function
+    :param start: sequence start
+    :param amount: number of steps
+    :return: generated next fibonacci number
+    """
+    a = start
+    b = start
+    for i in range(amount):
         yield a
         a,b = b,a+b
 
 
-for i in fibon(5):
+# start fibonacci from nubmer 5
+for i in fibon(50,10):
     print(i)
